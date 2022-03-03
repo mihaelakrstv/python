66e7b7ab57101ecb711aa52b75eda262e81bf2a8
mihaelakrstv <mihaelaxkr@abv.bg>
2022-03-03 19:23:25 +0200

    if exersices

---
+++ firstif.py
@@ -0,0 +1,6 @@
+x=int(input("Enter:x\n"))
+if x>0 :
+    print('Positive\n')
+else:
+    print('Negative\n')
+
---
+++ thirdif.py
@@ -0,0 +1,8 @@
+x=int(input('Enter x:\n'))
+if x>0 and x<40 :
+    print('First part')
+elif x>40 and x<80 :
+    print('Second part')
+else :
+    print('Third part')
+
